8d29cf291d235d504dd69ab3160dee7dcc6be6a4
Merwan <merwan@localhost.localdomain>
2020-08-09 10:41:36 +1000

    Files commited

---
+++ js/script.js
@@ -0,0 +1,56 @@
+console.log("js is connected");
+
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/*a*/
+/*b*/
+/*c*/
+/*d*/
+/*e*/
+/*f*/
+/*g*/
+/*h*/
+/*i*/
+/*j*/
+/*k*/
+/*l*/
+/*m*/
+/*n*/
+/*o*/
+/*p*/
+/*q*/
+/*r*/
+/*s*/
+/*t*/
+function thumbNailBuilder(url){
+	var imageParent = document.getElementById("result");
+	var nImg = document.createElement("img");
+		nImg.src = url;
+		nImg.style.width = "300px";
+		imageParent.appendChild(nImg);
+}
+/*u*/
+/*v*/
+/*w*/
+/*x*/
+/*y*/
+/*z*/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+
